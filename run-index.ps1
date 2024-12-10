0854561881987283e9afc0927f670d492e0313be
Steve Salas <salas@blackduck.com>
2024-12-09 21:02:38 -0500

    Update run-index.ps1 for Black Duck
    - specify securitycontext for MariaDB and MinIO charts

--- run-index.ps1
+++ run-index.ps1
@@ -1,1 +1,12 @@
+Push-Location $PSScriptRoot
+
 helm repo index . --url https://codedx.github.io/srm-k8s
+
+$index = Get-Content index.yaml
+$index | ForEach-Object {
+    $_ -replace 'https://github.com/synopsys-sig/srm-k8s','https://github.com/codedx/srm-k8s'
+} | ForEach-Object {
+    $_ -replace 'https://synopsys-sig.github.io/srm-k8s','https://codedx.github.io/srm-k8s'
+} | ForEach-Object {
+    $_ -replace 'https://sig-repo.synopsys.com/artifactory/sig-cloudnative','https://repo.blackduck.com/artifactory/sig-cloudnative'
+} | Out-File index.yaml
